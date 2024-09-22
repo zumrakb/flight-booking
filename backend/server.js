6d15a71206280f78e66a9cb95ee9007912ea0973
zumrakb <zumrakb8@gmail.com>
2024-09-22 23:36:49 +0300

    Implement production build

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,21 @@ const app = express();
 const port = process.env.PORT || 8080;
 const cors = require("cors");
 const connectDB = require("./config/db.js");
-
+const path = require("path");
 const apiV1 = require("./routes/apiV1.js");
 
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "build")));
 
 // route
 app.use("/api/v1", apiV1);
+
+// sending frontend files
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
 
 // listener
 app.listen(port, async () => {
